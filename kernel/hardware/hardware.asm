228ec8649970792d721f59ec1987fef4f43a247c
dual-core <131370751+dual-core1@users.noreply.github.com>
2023-06-09 20:08:02 -0500

    Delete kernel directory

--- kernel/hardware/hardware.asm
+++
@@ -1,8 +0,0 @@
-;---------------------------------------
-;HARDWARE
-;Consolidates all hardware-related code.
-;Part of the 64-DOS kernel.
-;---------------------------------------
-
-	include "keyboard.asm"
-	include "serial.asm"
